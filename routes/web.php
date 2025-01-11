fceb5cb14d95527c3382030c6139ec1e819e0787
Apri Singgih <aprisinggihwidodo@gmail.com>
2025-01-11 15:31:25 +0700

    build

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/login', [AuthenticationController::class, 'loginProcess'])->name('auth.login');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
