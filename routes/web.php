03b199ace2e86f920bf22db3e8cbcae351528ec6
elingsuryo <elingsuryo@gmail.com>
2025-01-11 13:49:35 +0700

    ui design login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/auth','template.auth');
+Route::view('/auth-login','auth.login');
