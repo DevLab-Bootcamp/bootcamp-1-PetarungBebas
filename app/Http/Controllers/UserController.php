f24cfb749ef5ff5cd3474284f9e22df65df58baa
Apri Singgih <aprisinggihwidodo@gmail.com>
2025-01-19 16:22:19 +0700

    feat(user): CRUD user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,7 @@ class UserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => 'PATIEN'
+            'role' => $request->role
         ]);
 
         $userProfile = UserProfile::create([
