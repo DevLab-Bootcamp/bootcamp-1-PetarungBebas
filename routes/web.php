7fd76322ee9da72192f81c1428dc023f32840a8a
elingsuryo <elingsuryo@gmail.com>
2025-01-19 14:00:54 +0700

    Tambah Dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::view('/auth','template.auth');
@@ -14,6 +14,5 @@ Route::view('/auth-login','auth.login');
 
 Route::post('/login', [AuthenticationController::class, 'login'])->name("auth.login");
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
 
