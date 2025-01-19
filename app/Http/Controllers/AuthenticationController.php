d4c1ee608304bf0ac6256960359cb9defee5802c
Apri Singgih <aprisinggihwidodo@gmail.com>
2025-01-19 12:09:23 +0700

    feat(auth): add login

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,21 +4,62 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use function App\Helpers\successResponse;
+use function App\Helpers\errorResponse;
 
 class AuthenticationController extends Controller
 {
-    //
-
-    public function login(Request $request){
-        $auth = Auth::attempt([
-            'email' => $request->email,
-            'password' => $request->password,
+    public function login(Request $request)
+    {
+        // Validasi input
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
 
-        if($auth){
-            return back()->with('error', 'Credential Anda Salah');
+        // Jika validasi gagal
+        if ($validator->fails()) {
+            if (!$request->input('email')) {
+                $errors['email'][] = 'The email field is required.';
+            }
+            if (!$request->input('password')) {
+                $errors['password'][] = 'The password field is required.';
+            }
+            return errorResponse('The credential field is required', $errors, 401);
         }
 
-        return redirect()->route('dashboard.index')->with('success', 'Berhasil Login');
+
+        // Siapkan kredensial login
+        $credentials = $request->only('email', 'password');
+
+        // Tentukan apakah credential berupa email atau username
+        if (filter_var($request->credential, FILTER_VALIDATE_EMAIL)) {
+            $credentials['email'] = $request->credential;
+        } else {
+            $credentials['username'] = $request->credential;
+        }
+
+        // Cek apakah kredensial valid
+        
+        if (!Auth::attempt($credentials)) {
+            $errors['errors'][] = 'The email or password is incorrect.';
+            return errorResponse('Invalid credentials', $errors, 401);
+        }
+        // Generate token JWT
+        $user = Auth::user();  // Ambil user yang berhasil login
+        $token = "JWTAuth::fromUser(user)";  // Membuat token untuk user tersebut
+
+        // Kirim response sukses dengan token
+        return successResponse('Login successful', [
+            'user' => $user, // Informasi user yang berhasil login
+            'token' => $token, // JWT Token
+        ]);
+
+    }
+
+    public function register(Request $request){
+        
     }
 }
