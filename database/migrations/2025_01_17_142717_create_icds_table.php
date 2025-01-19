c210573e9768c7f5a5dc2133ff4b4b68c17570ce
Apri Singgih <aprisinggihwidodo@gmail.com>
2025-01-19 16:06:07 +0700

    feat(migrate): add softdelete

--- database/migrations/2025_01_17_142717_create_icds_table.php
+++ database/migrations/2025_01_17_142717_create_icds_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name_en');
             $table->string('name_id');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
