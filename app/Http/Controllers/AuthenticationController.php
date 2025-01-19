9f83d868d6dc955d53757653c5489b9dad2ad464
Apri Singgih <aprisinggihwidodo@gmail.com>
2025-01-19 15:19:04 +0700

    feat(auth): add login

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -13,13 +13,12 @@ class AuthenticationController extends Controller
 {
     public function login(Request $request)
     {
-        // Validasi input
+
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        // Jika validasi gagal
         if ($validator->fails()) {
             if (!$request->input('email')) {
                 $errors['email'][] = 'The email field is required.';
@@ -30,8 +29,6 @@ class AuthenticationController extends Controller
             return errorResponse('The credential field is required', $errors, 401);
         }
 
-
-        // Siapkan kredensial login
         $credentials = $request->only('email', 'password');
 
         // Tentukan apakah credential berupa email atau username
@@ -40,26 +37,17 @@ class AuthenticationController extends Controller
         } else {
             $credentials['username'] = $request->credential;
         }
-
-        // Cek apakah kredensial valid
         
         if (!Auth::attempt($credentials)) {
             $errors['errors'][] = 'The email or password is incorrect.';
             return errorResponse('Invalid credentials', $errors, 401);
         }
-        // Generate token JWT
-        $user = Auth::user();  // Ambil user yang berhasil login
-        $token = "JWTAuth::fromUser(user)";  // Membuat token untuk user tersebut
+        $user = Auth::user(); 
+        $token = "JWTAuth::fromUser(user)";  
 
-        // Kirim response sukses dengan token
         return successResponse('Login successful', [
-            'user' => $user, // Informasi user yang berhasil login
-            'token' => $token, // JWT Token
-        ]);
+            'user' => $user, 
+        ],$token);
 
-    }
-
-    public function register(Request $request){
-        
     }
 }
